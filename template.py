ba72921d52a4420eb5fb0fd0d0fd193f7eab5a2a
Nik-Nikhil1910 <nikhilkondinya@gmail.com>
2024-08-21 06:53:26 +0530

    third commit

--- template.py
+++ template.py
@@ -1,1 +1,5 @@
-import pandas as pd
+import os
+import pathlib as Path
+import logging
+#this allows us to set the basic configuration of logging. Setiing the level to info ,means that only the events of level info and above will be logged i.e.,info,warning,error and critical.
+logging.basicConfig(level= logging.INFO, format='[%(asctime)s]: %(message)s')
